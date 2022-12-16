3508cd144f7611f3738a462053bbc48e1033a797
theJoseNeto <joseneto180201@gmail..com>
2022-12-16 14:41:13 -0300

    todos podem ser listadas

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import InputTodo from "./components/inputTodo";
+import ListTodos from "./components/listTodos";
 
 function App() {
   return (
     <div className="App">
       <InputTodo></InputTodo>
+      <ListTodos></ListTodos>
     </div>
   );
 }
--- client/src/components/inputTodo.js
+++ client/src/components/inputTodo.js
@@ -1,27 +1,30 @@
 import { useState } from "react"
 
 const InputTodo = () => {
-    const [description, setDescription] = useState("teste")
+    const [description, setDescription] = useState("")
 
     const onSubmitForm = async (e) => {
         e.preventDefault();
         const body = { description }
-        const response = await fetch("http://localhost:3333/todo", {
+        const response = await fetch("http://localhost:3333/todos", {
             method: "POST",
             headers: { "Content-type": "application/json" },
             body: JSON.stringify(body)
-        }); 
-        console.log(response);
-        
+        });
+
+        window.location = "/";
+
     }
     return <>
 
         <h1 className="text-center mt-05">Input todo</h1>
+
         <form className="d-flex mt-5" onSubmit={onSubmitForm}>
-            <input type="text" className="form-control" value={description} onChange={e => {
-                setDescription(e.target.value)
-            }} />
+
+            <input type="text" className="form-control" value={description} onChange={e => setDescription(e.target.value)} />
+
             <button className="btn btn-success">Add</button>
+
         </form>
     </>
 };
